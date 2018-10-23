f28eacc399aa247ab217c4af7ebe6088b1c2a22a
huanent <huanent@hotmail.com>
2018-10-23 22:58:54 +0800

    init

---
+++ HttpContentExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent httpContent, JsonSerializerSettings settings = null)
+        {
+            string content = await httpContent.ReadAsStringAsync();
+
+            if (settings == null) return JsonConvert.DeserializeObject<T>(content);
+
+            return JsonConvert.DeserializeObject<T>(content, settings);
+        }
+    }
+}
---
+++ JsonContent.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace System.Net.Http
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object o) : this(JsonConvert.SerializeObject(o))
+        { }
+
+        public JsonContent(string content) : base(content, Encoding.UTF8, "application/json")
+        {
+        }
+    }
+}
